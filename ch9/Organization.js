31d1e538282f91063fd51c2535b4685570da908f
Tim Tsai <sky22357168@gmail.com>
2020-08-02 13:13:36 +0800

    Reanme Organization property

--- ch9/Organization.js
+++ ch9/Organization.js
@@ -3,14 +3,14 @@ class Organization {
         this._title = data.title;
         this._country = data.country;
     }
-    get name() { return this._title; }
-    set name(aString) { this._title = aString; }
+    get title() { return this._title; }
+    set title(aString) { this._title = aString; }
     get country() { return this._country; }
     set country(aCountryCode) { this._country = aCountryCode; }
 }
 
 
-const organization = new Organization({title: "Acme Gooseberries", country: "GB"});
+const organization = new Organization({ title: "Acme Gooseberries", country: "GB" });
 
 
 
