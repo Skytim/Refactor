cdc2a413d0d6657cd9535c3f586021406512d569
Tim Tsai <tim.tsai@xuenn.com>
2020-08-04 15:42:51 +0800

    ChangeReference to value

---
+++ ch9/ChangeReferenceToReference.test.js
@@ -0,0 +1,6 @@
+const changeReferenceToReference = require('./ChangeReferenceToReference');
+
+test('Change Reference to Value', () => {
+    const orderData = new changeReferenceToReference.Order({ number: 1234, customer: 1000 });
+    expect(orderData.customer.id).toBe(1000);
+});
