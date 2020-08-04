91de0781414fefb81dee34d0ce011350dc1935ac
Tim Tsai <tim.tsai@xuenn.com>
2020-08-04 15:26:18 +0800

    Refactor Person consturcotr

--- ch9/ChangeReferenceToValue.js
+++ ch9/ChangeReferenceToValue.js
@@ -2,12 +2,17 @@ class Person {
     constructor() {
         this._telephoneNumber = new TelephoneNumber();
     }
-    get officeAreaCode() { return this._telephoneNumber.areaCode; }
-    set officeAreaCode(arg) { this._telephoneNumber.areaCode = arg; }
-    get officeNumber() { return this._telephoneNumber.number; }
-    set officeNumber(arg) { this._telephoneNumber.number = arg; }
+    get officeAreaCode() {return this._telephoneNumber.areaCode;}
+    set officeAreaCode(arg) {this._telephoneNumber = new TelephoneNumber(arg, this.officeNumber);}
+    get officeNumber() {return this._telephoneNumber.number;}
+    set officeNumber(arg) {this._telephoneNumber.number = arg;}
+    
 }
 class TelephoneNumber {
+    constructor(areaCode, number) {
+        this._areaCode = areaCode;
+        this._number = number;
+    }
     get areaCode() { return this._areaCode; }
     set areaCode(arg) { this._areaCode = arg; }
     get number() { return this._number; }
