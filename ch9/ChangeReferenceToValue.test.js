6f3cc28ca9d42389dda586002538dfda51616960
Tim Tsai <tim.tsai@xuenn.com>
2020-08-04 15:36:22 +0800

    TelepnoneNumber equals

--- ch9/ChangeReferenceToValue.test.js
+++ ch9/ChangeReferenceToValue.test.js
@@ -4,4 +4,6 @@ test('Change Reference to Value', () => {
     const personData = new changeReferenceToValue.Person();
     personData.officeNumber = 100;
     expect(personData.officeNumber).toBe(100);
+    // expect(new changeReferenceToValue.TelephoneNumber("312", "555-0142")).toBe(new changeReferenceToValue.TelephoneNumber("312", "555-0142"));
+    expect(new changeReferenceToValue.TelephoneNumber("312", "555-0142").equals((new changeReferenceToValue.TelephoneNumber("312", "555-0142")))).toBe(true);
 });
